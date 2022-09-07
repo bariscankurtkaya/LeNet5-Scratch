dd84af2e6246beec0b9f9f17b81898a40b6ac364
ais-lab <bariscankurtkaya@gmail.com>
2022-09-07 15:29:19 +0200

    classes refactor

--- src/cnn/type.py
+++ src/cnn/type.py
@@ -1,9 +1,9 @@
-from typing import Literal
+from typing import Literal, Dict, List, TypedDict
 import numpy as np
 
-IMG: np.ndarray
-KERNEL: np.ndarray
-SCALARS: np.ndarray
+IMG= np.ndarray
+KERNEL= np.ndarray
+SCALARS= np.ndarray
 
 correlation_mode = Literal["full", "valid", "same"]
 boundary = Literal["fill", "wrap", "symm"]
@@ -12,75 +12,68 @@ activation_funcs = Literal["leaky", "relu", "sigmoid", "softmax"]
 pooling = Literal["max", "mean", None]
 
 # Layers
-conv_layer: dict = {
-    "kernel": KERNEL,
-    "bias": KERNEL,
-    "activation": str,
-    "pooling": Literal["max", "mean", None]
-}
+class conv_layer(TypedDict):
+    kernel: KERNEL
+    bias: KERNEL
+    activation: str
+    pooling: pooling
 
-fc_layer: dict = {
-    "weight": SCALARS,
-    "bias": SCALARS,
-    "activation": str
-}
+class fc_layer(TypedDict):
+    weight: SCALARS
+    bias: SCALARS
+    activation: str
 
-fc_layers: dict[fc_layer]
-conv_layers: dict[conv_layer]
 
+class fc_layers(TypedDict):
+    fc_layers: List[fc_layer]
+
+class conv_layers(TypedDict):
+    conv_layers: List[conv_layer]
 
 
 # Network
-network: dict = {
-    "conv_layers": conv_layers,
-    "fc_layers": fc_layers
-} 
+class network(TypedDict):
+    conv_layers: conv_layers
+    fc_layers: fc_layers
 
 
 
 # Caches
-conv_cache: dict = {
-    "conv_inputs" : IMG,
-    "last_output" : IMG
-}
+class conv_cache(TypedDict):
+    conv_inputs: IMG
+    last_output: IMG
 
-fc_cache: dict = {
-    "activation_outputs": IMG, #A
-    "layer_outputs" : IMG #Z
-}
+class fc_cache(TypedDict):
+    activation_outputs: IMG #A
+    layer_outputs: IMG #Z
 
-forward_cache: dict = {
-    "conv_cache" : conv_cache,
-    "fc_cache" : fc_cache,
-    "loss": np.ndarray
-}
+class forward_cache(TypedDict):
+    conv_cache: conv_cache 
+    fc_cache: fc_cache 
+    loss: np.ndarray
 
+class bconv_cache(TypedDict):
+    kernel_derivs: KERNEL
+    bias_derivs: KERNEL
 
-bfc_cache: dict = {
-    "weight_derivs": SCALARS,
-    "bias_derivs" : SCALARS,
-    "last_deriv" : SCALARS
-}
+class bfc_cache(TypedDict):
+    weight_derivs: SCALARS
+    bias_derivs: SCALARS
+    last_deriv : SCALARS
 
-bconv_cache: dict = {
-    "kernel_derivs": KERNEL,
-    "bias_derivs": KERNEL
+class backward_cache(TypedDict):
+    bfc_cache: bfc_cache 
+    bconv_cache: bconv_cache 
 
-}
-
-
-backward_cache: dict = {
-    "bfc_cache" : bfc_cache,
-    "bconv_cache" : bconv_cache
-}
 
 
 # Dataset
-dataset: dict = {
-    "name": str,
-    "input": np.ndarray,
-    "target": np.ndarray
-}
+class dataset(TypedDict):
+    name: str
+    input: IMG
+    target : SCALARS
+
+
 
 
 
